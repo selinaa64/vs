59065b04d0339f69ec279dfe6113353a162cd518
Selina Moritz <semoit00@hs-esslingen.de>
2023-05-07 18:10:28 +0200

    ordner

--- src/main/java/com/example/demo/controller/ApiController.java
+++ src/main/java/com/example/demo/controller/ApiController.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.example.demo.controller;
 
+import com.example.demo.service.ServiceTodoItem;
+import com.example.demo.model.TodoItem;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
--- src/main/java/com/example/demo/service/ServiceTodoItem.java
+++ src/main/java/com/example/demo/service/ServiceTodoItem.java
@@ -1,5 +1,6 @@
-package com.example.demo;
+package com.example.demo.service;
 
+import com.example.demo.model.TodoItem;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
