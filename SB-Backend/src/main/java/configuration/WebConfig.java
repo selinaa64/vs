eb734dffe6296325bf4f5f00289a7cf1bd303759
Selina Moritz <semoit00@hs-esslingen.de>
2023-05-17 20:46:58 +0200

    port fix

--- SB-Backend/src/main/java/configuration/WebConfig.java
+++ SB-Backend/src/main/java/configuration/WebConfig.java
@@ -1,3 +1,5 @@
+package configuration;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
